ab5b3b14449029b06501dd48e4f5bd631f60b8ad
natooni <100316459+Nathan-Duong@users.noreply.github.com>
2024-04-17 23:55:12 -0700

    More errors...

--- pythonProject/nathan.py
+++ pythonProject/nathan.py
@@ -69,7 +69,7 @@ class Token:
         self.value = value
 
         if pos_start:
-            self.post_start = pos_start.copy()
+            self.pos_start = pos_start.copy()
             self.pos_end = pos_start.copy()
             self.pos_end.advance()
 
